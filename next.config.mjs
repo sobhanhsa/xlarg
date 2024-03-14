9cd6e293ab930812d399c26ed2c1c0816627288e
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-14 20:03:59 +0330

    fix:reexport config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,6 @@ const nextConfig = {
     images:{
         domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
     }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
