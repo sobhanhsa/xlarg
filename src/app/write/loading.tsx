be5371513c4f31bfdda9a3919e037324ce55cb0d
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-14 19:58:31 +0330

    feat:use loading component

---
+++ src/app/write/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingComponent from "@/components/loadingComponent/LoadingComponent";
+
+
+const WritePageLoading  = () => {
+    return (
+        <LoadingComponent/>
+    )
+};
+
+export default WritePageLoading;
