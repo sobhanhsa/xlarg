295ecc0cfc5073217f56ac046a11ac60de42dbf6
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-14 22:15:27 +0330

    fix:edit firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -8,14 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE,
-  authDomain: "medium-blog-app.firebaseapp.com",
-  projectId: "medium-blog-app",
-  storageBucket: "medium-blog-app.appspot.com",
-  messagingSenderId: "22535067998",
-  appId: "1:22535067998:web:d06194ca2b5baca77997cf",
-  measurementId: "G-VZYR0S1E5Z"
+  authDomain: "xlarg-417209.firebaseapp.com",
+  projectId: "xlarg-417209",
+  storageBucket: "xlarg-417209.appspot.com",
+  messagingSenderId: "197452823797",
+  appId: "1:197452823797:web:af01b3319dc4470d8bd9bc",
+  measurementId: "G-Z0D4XW58V0"
 };
 
 // Initialize Firebase
 export const fireBaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(fireBaseApp);
