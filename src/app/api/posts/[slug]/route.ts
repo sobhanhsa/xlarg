29fc22cbfd56857ac21426cb02b9d4a3d810d56d
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-15 22:00:09 +0330

    feat:implement delete logic

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -5,6 +5,7 @@ import { where } from "firebase/firestore"
 import { NextRequest, NextResponse } from "next/server"
 
 export const GET = async(req:NextRequest,{params}:any) => {
+    
     try {
 
         // const user : UserType | undefined = await getAuthSession() as UserType | undefined
@@ -46,6 +47,75 @@ export const GET = async(req:NextRequest,{params}:any) => {
                     {
                         message:"something went wrong!", 
                         erorrMassage:error["message"],
+                    },{
+                        status:500,
+                    } as any
+                )
+            );
+    }
+
+}
+
+export const DELETE = async(req:NextRequest,{params}:{params:{slug:string}}) => {
+
+    const session = await getAuthSession();
+
+    if (!session) {
+        return new NextResponse(JSON.stringify(
+            {
+                message:"not authenticated!"
+            },{
+                status:401
+            } as any)
+        )
+    };
+
+    try {
+        
+        const post = await prisma.post.findUnique({
+            where:{
+                slug:params.slug
+            },
+            include:{
+                user:true
+            }
+            
+        })
+        
+        if (post?.user.email !== (session as any).user.email){
+            return new NextResponse(JSON.stringify(
+                {
+                    message:"you don't have access to this post"
+                },{
+                    status:401,
+                } as any
+                )
+            );
+        }   
+
+        console.log(post?.id);
+
+        const deletedPost = await prisma.post.delete({
+            where:{
+                id:post?.id
+            }
+        });
+
+        return new NextResponse(JSON.stringify(
+                {
+                    deletedPost
+                },{
+                    status:200,
+                } as any
+            )
+        );
+    }
+    catch(error : any&Error){
+        console.error(error)
+        return new NextResponse(
+                JSON.stringify(
+                    {
+                        message:error["message"], 
                     },{
                         status:500,
                     } as any
