3a3c87224fe32a97bf62f6b0ab3b2a7948cdaa1c
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-15 22:02:15 +0330

    feat:implement edit front

---
+++ src/app/edit/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./editPage.module.css"
+
+const  EditPage = () => {
+    return (
+        <div className={styles.container}>
+            EditPage   
+        </div>
+    )
+};
+
+export default EditPage;
