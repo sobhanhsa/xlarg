f85855e46dc6b688a9c61fd706169f8a891cc505
sobhanhsa <sobhanb7b7@protonmail.com>
2024-03-15 13:01:03 +0330

    fix:edit next confing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
+        remotePatterns:[
+            {
+                protocol:"https",
+                hostname:"lh3.googleusercontent.com"
+            },
+            {
+                protocol:"https",
+                hostname:"firebasestorage.googleapis.com"
+            }
+        ]
     }
 };
 
